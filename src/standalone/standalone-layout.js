159594aa1a59c2e079a68fb72a9035aeb09b16aa
rreexxllii0310 <rreexxllii0310@gmail.com>
2022-06-03 01:50:29 +0800

    remove the NavBar component of swagger-editor

--- src/standalone/standalone-layout.js
+++ src/standalone/standalone-layout.js
@@ -11,11 +11,9 @@ export default class StandaloneLayout extends React.Component {
   render() {
     const { getComponent } = this.props
     const EditorLayout = getComponent("EditorLayout", true)
-    const Topbar = getComponent("Topbar", true)
 
     return (
       <div>
-        <Topbar />
         <EditorLayout/>
       </div>
     )
